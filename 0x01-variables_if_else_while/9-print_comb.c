357d97a8eb2fd90de364c9aef15c47490668a059
Dinarwanjiru <dinarwanjiru77@gmail.com>
2023-06-16 12:33:59 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints ll possible combinations of single-digit numbers
+ * Return: Always
+ */
+int main(void)
+{
+int s;
+for (s='0'; s <= '9'; s++)
+{
+putchar(s);
+if (d != '9')
+{
+putchar( ',')
+putchar(' ')
+}
+}
+return (0);
+}
+
