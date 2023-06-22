7865673be3950739d2fe85fa228ce0128f2cb1f3
Dinarwanjiru <dinarwanjiru77@gmail.com>
2023-06-22 11:48:22 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - prints numbers, from 0 to 9,
- * except 2 and 4,plus followed by a new line
+ * except 2 and 4, plus followed by a new line
  */
 void print_most_numbers(void)
 {
