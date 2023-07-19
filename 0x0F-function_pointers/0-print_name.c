d1317dfe1ac2c6b3a16594c0b2329f648edf1e61
Dinarwanjiru <dinarwanjiru77@gmail.com>
2023-07-19 04:40:51 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,12 @@
 /**
  * print_name - function to print name
- *
  * @name: name to print
  * @f: function pointer that doesn't return anything
- *
  * Return: Always 0 (Success)
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f) /* if any is NULL */
-		return;
-
-	f(name);
+	if (name && f)
+		f(name);
 }
