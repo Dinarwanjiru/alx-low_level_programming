2d21844ddb5215a1b96666347d4b2985476abdd3
dinarwanjiru <dinarwanjiru77@gmail.com>
2023-09-04 15:14:54 -0700

    fil2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,25 +1,26 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file nd prints it to the POSIX stdout
- * @filename: it is the name of the file that is being read
- * @letters: number of letters thst should be read and printed
- * Return: the actual number of letters read and printed, or 0 if fail
+ * create_file - function that creates file
+ * @filename: the name of the file that is being created
+ * @text_content: - string to write the file
+ * Return: 1 for success,-1 for failure
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
-	ssize_t a, b;
-	int abc;
-	char *buffer;
+	ssize_t abc = 0, a = 0, b = 0;
 
 	if (filename == NULL)
-		return (0);
-	abc = open(filename, O_RDONLY);
-	if (abc == -1)
-		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	a = read(abc, buffer, letters);
-	b = write(STDOUT_FILENO, buffer, a);
-		free(buffer);
-		close(abc);
-		return (b);
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	abc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (abc < 0)
+		return (-1);
+	while (text_content[b])
+		b++;
+	a = write(abc, text_content, b);
+	if (a < 0)
+		return (-1);
+	close(abc);
+	return (1);
 }
