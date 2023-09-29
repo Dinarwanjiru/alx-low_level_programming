214286ebd436d40c0b45f03706954521751be504
dinarwanjiru <dinarwanjiru77@gmail.com>
2023-09-29 06:20:40 -0700

    file1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - print the nmber of arguments
+* @argc: Count arguments
+* @argv: Arguments
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
