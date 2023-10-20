a71ce4bb7122a148725730b33a04fbf77d635f74
dinarwanjiru <dinarwanjiru77@gmail.com>
2023-10-20 12:22:13 -0700

    file0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,26 +5,24 @@
  * @h: pointer to head first node
  * Return: number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t count = 1;
+	size_t node_count = 1;
 
 	if (h == NULL)
 		return (0);
+
 	while (h->next != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[%d] %s\n", 0, "(nil)");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
+
 		h = h->next;
-		count += 1;
+		node_count += 1;
 	}
 	printf("[%d] %s\n", h->len, h->str);
-	return (count);
+
+	return (node_count);
 }
